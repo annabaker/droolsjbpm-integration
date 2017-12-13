33e41378be4d7d60925679fa95886fc0c1bbc34f
Anna Baker <anbaker@redhat.com>
2017-12-12 22:17:59 -0500

    things

---
+++ kie-server-parent/kie-server-tests/kie-server-integ-tests-jbpm/src/test/filtered-resources/kjars-sources/jms-wih-project/src/main/java/org/jbpm/data/HelloWorkItemHandler.java
@@ -0,0 +1,19 @@
+package org.jbpm.data;
+
+import org.drools.core.process.instance.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemManager;
+
+public class HelloWorkItemHandler implements WorkItemHandler {
+
+	public void abortWorkItem(WorkItem wi, WorkItemManager wim) {
+		System.out.println("Oh no, my item aborted..");
+
+	}
+
+	public void executeWorkItem(WorkItem wi, WorkItemManager wim) {
+		System.out.println("Hello World!");
+		wim.completeWorkItem(wi.getId(), null);
+	}
+
+}
